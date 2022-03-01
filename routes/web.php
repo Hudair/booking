bfaa8212c76d8865e7226d379335492ebce0f22e
Mohammed Hudair <47048229+Hudair@users.noreply.github.com>
2022-03-01 22:15:42 +0300

    upload project

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
+Route::get('/intro','LandingpageController@index');
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/install/check-db', 'HomeController@checkConnectDatabase');
+
+Route::get('/update', function (){
+    return redirect('/');
+});
+
+
+//Custom User Login and Register
+
+Route::post('register','\Modules\User\Controllers\UserController@userRegister')->name('auth.register');
+Route::post('logout','\Modules\User\Controllers\UserController@logout')->name('auth.logout');
+// Social Login
+Route::get('social-login/{provider}', 'Auth\LoginController@socialLogin');
+Route::get('social-callback/{provider}', 'Auth\LoginController@socialCallBack');
+
+// Logs
+Route::get(config('admin.admin_route_prefix').'/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware(['auth', 'dashboard','system_log_view'])->name('admin.logs');
